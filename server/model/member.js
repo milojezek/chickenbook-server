ee54a704c982a1a5899a8897322144ab1814c9f3
milojezek <milo.jezek@gmail.com>
2024-02-03 18:59:00 +0100

    Create member model

---
+++ server/model/member.js
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+
+const memberSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  headline: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: Array,
+    required: true
+  },
+  projects: {
+    type: Array,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  gitHub: {
+    type: String,
+    required: true
+  },
+  linkedIn: {
+    type: String,
+    required: true
+  },
+  updateAt: {
+    type: Date,
+    required: true
+  }
+});
+
+const memberModel = model("member", memberSchema);
+
+export default memberModel;
